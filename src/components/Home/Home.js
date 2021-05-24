305c36c4f64d8eb288d8fd54ce1b2cc79686ab3f
Ankit Raj <2019ugcs001@nitjsr.ac.in>
2021-05-24 17:41:49 +0530

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,153 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import './Home.css';
+import io from 'socket.io-client';
+import ScrollToBottom from 'react-scroll-to-bottom';
+const ENDPOINT = 'http://localhost:5000/'
+const socket = io(ENDPOINT)
+
+function Home() {
+    const [friends, setFriends] = useState([])
+    const [roomId, setRoomId] = useState('')
+    const [messages, setMessages] = useState([])
+    const [message, setMessage] = useState('');
+    const [searchText, setSearchText] = useState('')
+
+    useEffect(()=>{
+        axios.get(`http://localhost:5000/get_friends/${localStorage.getItem('userId')}`)
+        .then((res)=>{
+            console.log(res.data.friends)
+            setFriends(res.data.friends)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    },[])
+
+    const openChat = (friend) =>{
+        friend.friends.map((_)=>{
+            if(_.friendId.toString() === localStorage.getItem('userId').toString()){
+                setRoomId(_.roomId.toString())
+            }
+        })
+    }
+
+    useEffect(()=>{
+        axios.get(`http://localhost:5000/get_room/${roomId}`)
+        .then((res)=>{
+            console.log('room',res?.data?.room)
+
+            const m = res?.data?.room?.messages.map(m=>{
+                return {message:m.message, user:m.user}
+            })
+            console.log('messages', m)
+            setMessages(m)
+            socket.emit('join',{roomId:roomId},()=>{});
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    },[roomId])
+
+    useEffect(()=>{
+        socket.on('message',(message)=>{
+            if(messages){
+                setMessages([...messages, message])
+            }
+            else{
+                setMessages([message])
+            }
+        })
+    },[messages])
+
+    //function to send message
+    const sendMessage = (e) => {
+        e.preventDefault();
+        if(message){
+            let userId = localStorage.getItem('userId');
+            socket.emit('sendMessage', {message:message, userId: userId,roomId: roomId},()=>{setMessage('')})
+        }
+    }
+
+    const search = (e) =>{
+        e.preventDefault();
+        axios.post('http://localhost:5000/search',{searchText})
+    }
+    return (
+        <div className='home-container'>
+            <div className='home-body'>
+                <div className='home-body-left'>
+                    <div className='home-header'>
+                        header
+                    </div>
+                    <div className='home-search'>
+                        <input 
+                            className='search'
+                            type="text" 
+                            value={searchText}
+                            onChange={e=>setSearchText(e.target.value)}
+                            onKeyPress={event => event.key === 'Enter' ? search(event) : null}
+                            placeholder="Search friends"
+                        />
+                        <button className="search-button" onClick={e => search(e)}>Search</button>
+                    </div>
+                    <div className='home-friends'>
+                        {
+                            friends.map(friend => {
+                                return <p onClick={()=>openChat(friend)} key={friend._id}>{friend.username}</p>
+                            })
+                        }
+                    </div>
+                </div>
+                <div className='home-body-right'>
+                    <div className='home-header'>
+                        header
+                    </div>
+
+                    {
+                        roomId ? (
+                            <>
+                            <div className='home-chat'>
+                                <ScrollToBottom>
+                                {
+                                    messages?.map((msg,i)=>{
+                                        return (
+                                            msg.user.toString() === localStorage.getItem('userId').toString() ? (
+                                                <div className="message-container justifyEnd" key={i}>
+                                                    <p className="message user">{msg.message}</p>
+                                                </div>
+                                            ):(
+                                                <div className="message-container justifyStart" key={i}>
+                                                    <p className="message friend">{msg.message}</p>
+                                                </div>
+                                            )
+                                        
+                                        )
+                                    })
+                                }
+                                </ScrollToBottom>
+
+                            </div>
+                            <div className='home-input-container'>
+                                <input type='text' 
+                                    className='home-input'
+                                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+                                    placeholder='type here ... ' 
+                                    value={message} 
+                                    onChange={e=>setMessage(e.target.value)}
+                                />
+                                <button className="home-input-button" onClick={e => sendMessage(e)}>Send</button>
+                            </div>
+                            </>
+                        ):(
+                            <></>
+                        )
+                    }
+                </div>
+                    
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Join/Join.js
@@ -0,0 +1,92 @@
+import React,{useState} from 'react'
+import './Join.css'
+import axios from 'axios';
+import {useHistory} from 'react-router-dom'
+function Join() {
+
+    const history = useHistory();
+    const [password ,setPassword] = useState('');
+    const [username ,setUsername] = useState('');
+    const [isLogin, setIsLogin] = useState(true);
+    const [message, setMessage] = useState('')
+
+    const login = (e) => {
+      e.preventDefault();
+      axios.post("http://localhost:5000/login",{
+        username,
+        password
+      })
+      .then((res)=>{
+        console.log('login',res.data);
+        localStorage.clear()
+
+        if(res.data.status){
+          localStorage.setItem('token', res.data.token)
+          localStorage.setItem('userId', res.data.user._id)
+          localStorage.setItem('username', res.data.user.username);
+          history.push('/home');
+        }
+        else{
+          setMessage(res.data.message);
+          alert(res.data.message);
+        }
+      })
+      .catch(err => {
+        console.log('error',err);
+      })
+
+    }
+
+    const signup = (e) => {
+      e.preventDefault();
+      axios.post("http://localhost:5000/register",{
+        username,
+        password
+      })
+      .then((res)=>{
+        console.log('login',res.data);
+        localStorage.clear()
+        if(res.data.status){
+          localStorage.setItem('token', res.data.token)
+          localStorage.setItem('userId', res.data.user._id)
+          localStorage.setItem('username', res.data.user.username);
+          history.push('/home');
+        }
+        else{
+          setMessage(res.data.message);
+          alert(res.data.message);
+        }
+      })
+      .catch(err => {
+        console.log('error',err);
+      })
+    }
+
+    return (
+        <div className='joinOuterContainer'>
+            <div className='joinInnerContainer'>
+              {
+                isLogin ? (
+                  <>
+                    <h1 className='heading'>Login</h1>
+                    <div><input placeholder="Username" className="joinInput" type="text" onChange={e=>setUsername(e.target.value)}/></div>
+                    <div><input placeholder="Password" className="joinInput mt-20" type="password" onChange={e=>setPassword(e.target.value)}/></div>
+                    <button className='button mt-20' onClick={login} type='submit'>Login In</button>
+                    <p style={{color:"white",cursor:"pointer"}}onClick={()=>setIsLogin(false)}>Switch to Sign Up</p>
+                  </>
+                ):(
+                  <>
+                    <h1 className='heading'>Sign Up</h1>
+                    <div><input placeholder="Username" className="joinInput" type="text" onChange={e=>setUsername(e.target.value)}/></div>
+                    <div><input placeholder="Password" className="joinInput mt-20" type="password" onChange={e=>setPassword(e.target.value)}/></div>
+                    <button className='button mt-20' onClick={signup} type='submit'>Sign Up</button>
+                    <p style={{color:"white", cursor:"pointer"}} onClick={()=>setIsLogin(true)}>Switch to Log in</p>
+                  </>
+                )
+              }
+            </div>
+        </div>
+    )
+}
+
+export default Join
